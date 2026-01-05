944954edc99f73b4b872ca30e78fee69985fa772
arovchinnikov <arovchinnikov@icloud.com>
2026-01-05 14:36:19 +0300

    epoch manager commands update

--- src/main/java/io/github/insomniacteam/skyforgeddreams/worldstate/EpochSavedData.java
+++ src/main/java/io/github/insomniacteam/skyforgeddreams/worldstate/EpochSavedData.java
@@ -10,21 +10,29 @@ import org.jetbrains.annotations.NotNull;
 public class EpochSavedData extends SavedData {
     private static final String DATA_NAME = "skyforged_dreams_epoch";
     private static final String TAG_CURRENT_EPOCH = "CurrentEpoch";
+    private static final String TAG_NEXT_EPOCH = "NextEpoch";
     private static final String TAG_TICKS = "TicksInEpoch";
     private static final String TAG_LAST_CHECKED_TIME = "LastCheckedTime";
 
     private WorldEpoch currentEpoch;
+    private WorldEpoch nextEpoch;
     private long ticksInCurrentEpoch;
     private long lastCheckedTime;
 
     public EpochSavedData() {
         this.currentEpoch = Config.startingEpoch;
+        this.nextEpoch = null; // Will be set when needed
         this.ticksInCurrentEpoch = 0;
         this.lastCheckedTime = 0;
     }
 
     public EpochSavedData(CompoundTag tag, HolderLookup.Provider provider) {
         this.currentEpoch = WorldEpoch.valueOf(tag.getString(TAG_CURRENT_EPOCH));
+        if (tag.contains(TAG_NEXT_EPOCH)) {
+            this.nextEpoch = WorldEpoch.valueOf(tag.getString(TAG_NEXT_EPOCH));
+        } else {
+            this.nextEpoch = null;
+        }
         this.ticksInCurrentEpoch = tag.getLong(TAG_TICKS);
         this.lastCheckedTime = tag.getLong(TAG_LAST_CHECKED_TIME);
     }
@@ -32,6 +40,9 @@ public class EpochSavedData extends SavedData {
     @Override
     public @NotNull CompoundTag save(@NotNull CompoundTag tag, HolderLookup.@NotNull Provider provider) {
         tag.putString(TAG_CURRENT_EPOCH, currentEpoch.name());
+        if (nextEpoch != null) {
+            tag.putString(TAG_NEXT_EPOCH, nextEpoch.name());
+        }
         tag.putLong(TAG_TICKS, ticksInCurrentEpoch);
         tag.putLong(TAG_LAST_CHECKED_TIME, lastCheckedTime);
         return tag;
@@ -54,6 +65,15 @@ public class EpochSavedData extends SavedData {
 
     public void setCurrentEpoch(WorldEpoch epoch) {
         this.currentEpoch = epoch;
+        setDirty();
+    }
+
+    public WorldEpoch getNextEpoch() {
+        return nextEpoch;
+    }
+
+    public void setNextEpoch(WorldEpoch epoch) {
+        this.nextEpoch = epoch;
         setDirty();
     }
 
